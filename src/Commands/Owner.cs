67f1b0a626174b3f4925cb41dff5d3788b555ccd
Mariano Burguete <marianoburguete@gmail.com>
2024-07-08 22:21:16 -0300

    test

--- src/Commands/Owner.cs
+++ src/Commands/Owner.cs
@@ -32,8 +32,7 @@
         public async Task Test(InteractionContext ctx)
         {
             await ctx.DeferAsync();
-            var value = await MediaUserQuery.GetMediaFromUser(ctx, 1, 1);
-            await ctx.DeleteResponseAsync();
+            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Test!"));
         }
 
         [SlashCommand("guild", "Information about a server")]
